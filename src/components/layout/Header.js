52cb205662839411f970f2f21286facfa8e2b621
Sailor1314 <zhongfeng.wu@edu.sait.ca>
2024-04-14 13:55:30 -0600

    fixed the tsconfig.json file' s path problem
    I created a route.ts file, the automatic generated a new file that was tsconfig.json, I need to add a path to tsconfig.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
           <Link href={'/register'} className="bg-primary rounded-full
            text-white px-4 py-2">
               Register
-            </Link>
+          </Link>
         </nav>
       </header>
     );
